303f74a9a399cdc273d07273dcba161cd26d30a9
Patnayak, Nilikh <Nilikh.Patnayak@searshc.com>
2017-12-04 17:13:00 -0600

    IBDesignable and IBInspectable https://youtu.be/yI-EY6MP9So

---
+++ UIConcepts/IBDesignable/BorderedButton.swift
@@ -0,0 +1,31 @@
+//
+//  BorderedButton.swift
+//  UIConcepts
+//
+//  Created by Patnayak, Nilikh on 12/4/17.
+//  Copyright © 2017 Patnayak, Nilikh. All rights reserved.
+//
+
+import UIKit
+
+ @IBDesignable class BorderedButton: UIButton {
+
+    @IBInspectable var borderWidth: CGFloat = 0.0 {
+        didSet {
+            self.layer.borderWidth = borderWidth
+        }
+    }
+    
+    @IBInspectable var borderColor: UIColor = .clear {
+        didSet {
+            self.layer.borderColor = borderColor.cgColor
+        }
+    }
+    
+    @IBInspectable var cornerRadius: CGFloat = 0.0 {
+        didSet {
+            self.layer.cornerRadius = cornerRadius
+        }
+    }
+
+}
